555dcdfdf016136e2615f66dd9a6c80ba39e5c33
HermansJarno <jarnohermans94@gmail.com>
2016-04-21 21:35:36 +0200

    MobileInputScript And Begin PaperController

---
+++ App/projectwerking/Assets/Scripts/PaperController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PaperController : MonoBehaviour {
+
+  public GameObject paper;
+  public Text paperText;
+  public Transform startposition, focusposition, endposition;
+
+  Rigidbody currentPaper;
+  int currentQuestionNr = 0;
+  string[] paperTextArray;
+  
+	// Use this for initialization
+	void Start () {
+    paperText.text = paperTextArray[0];
+    currentPaper = (Rigidbody)Instantiate(paper, focusposition.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  
+	}
+
+  void setNextPaperText() {
+    currentQuestionNr++;
+    paperText.text = paperTextArray[currentQuestionNr];
+  }
+
+  void setPreviousPaperText() {
+    currentQuestionNr--;
+    paperText.text = paperTextArray[currentQuestionNr];
+  }
+
+  public void SwipeNext() {
+
+  }
+
+  public void SwipePrevious() {
+
+  }
+}
