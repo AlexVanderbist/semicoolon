733d738f59a49cf772bbb87e97b2a7c17c305e6e
rubends <rubendeswaef@gmail.com>
2016-04-24 14:55:42 +0100

    added more translations

---
+++ web/resources/lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Wachtwoorden moeten tenminste 6 tekens hebben en overeenkomen met de bevestiging.',
+    'reset' => 'Je wachtwoord is gereset!',
+    'sent' => 'We hebben u de wachtwoordreset link gemaild!',
+    'token' => 'Dit wachtwoord reset token is ongeldig.',
+    'user' => "We kunnen geen gebruiker met dat e-mailadres vinden.",
+
+];
---
+++ web/resources/lang/nl/validation.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted'             => ':attribute moet geaccepteerd worden.',
+    'active_url'           => ':attribute is geen geldige URL.',
+    'after'                => ':attribute moet een dag zijn na :date.',
+    'alpha'                => ':attribute mag enkel letters bevatten.',
+    'alpha_dash'           => ':attribute may only contain letters, numbers, and dashes.',
+    'alpha_num'            => ':attribute mag enkel letters en cijfers bevatten.',
+    'array'                => ':attribute must be an array.',
+    'before'               => ':attribute must be a date before :date.',
+    'between'              => [
+        'numeric' => ':attribute moet tussen :min en :max zijn.',
+        'file'    => ':attribute moet tussen :min en :max kilobytes zijn.',
+        'string'  => ':attribute moet tussen :min en :max karakters zijn.',
+        'array'   => 'The :attribute must have between :min and :max items.',
+    ],
+    'boolean'              => 'The :attribute field must be true or false.',
+    'confirmed'            => 'The :attribute confirmation does not match.',
+    'date'                 => ':attribute is geen geldige datum.',
+    'date_format'          => 'The :attribute does not match the format :format.',
+    'different'            => ':attribute en :other moet verschillend zijn.',
+    'digits'               => 'The :attribute must be :digits digits.',
+    'digits_between'       => 'The :attribute must be between :min and :max digits.',
+    'email'                => ':attribute moet een geldig e-mailadres zijn',
+    'exists'               => 'The selected :attribute is invalid.',
+    'filled'               => ':attribute is verplicht.',
+    'image'                => ':attribute moet een foto zijn.',
+    'in'                   => 'The selected :attribute is invalid.',
+    'integer'              => 'The :attribute must be an integer.',
+    'ip'                   => 'The :attribute must be a valid IP address.',
+    'json'                 => 'The :attribute must be a valid JSON string.',
+    'max'                  => [
+        'numeric' => 'The :attribute may not be greater than :max.',
+        'file'    => 'The :attribute may not be greater than :max kilobytes.',
+        'string'  => 'The :attribute may not be greater than :max characters.',
+        'array'   => 'The :attribute may not have more than :max items.',
+    ],
+    'mimes'                => 'The :attribute must be a file of type: :values.',
+    'min'                  => [
+        'numeric' => 'The :attribute must be at least :min.',
+        'file'    => 'The :attribute must be at least :min kilobytes.',
+        'string'  => 'The :attribute must be at least :min characters.',
+        'array'   => 'The :attribute must have at least :min items.',
+    ],
+    'not_in'               => 'The selected :attribute is invalid.',
+    'numeric'              => ':attribute moet een nummer zijn.',
+    'regex'                => 'The :attribute format is invalid.',
+    'required'             => ':attribute is verplicht.',
+    'required_if'          => 'The :attribute field is required when :other is :value.',
+    'required_unless'      => 'The :attribute field is required unless :other is in :values.',
+    'required_with'        => 'The :attribute field is required when :values is present.',
+    'required_with_all'    => 'The :attribute field is required when :values is present.',
+    'required_without'     => 'The :attribute field is required when :values is not present.',
+    'required_without_all' => 'The :attribute field is required when none of :values are present.',
+    'same'                 => 'The :attribute and :other must match.',
+    'size'                 => [
+        'numeric' => 'The :attribute must be :size.',
+        'file'    => 'The :attribute must be :size kilobytes.',
+        'string'  => 'The :attribute must be :size characters.',
+        'array'   => 'The :attribute must contain :size items.',
+    ],
+    'string'               => 'The :attribute must be a string.',
+    'timezone'             => 'The :attribute must be a valid zone.',
+    'unique'               => ':attribute is al in gebruik.',
+    'url'                  => 'The :attribute format is invalid.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [],
+
+];
