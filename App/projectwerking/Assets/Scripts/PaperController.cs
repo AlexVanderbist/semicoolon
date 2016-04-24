c305ca9afa7b226ec67b89fed156e5e9ccf25a7c
HermansJarno <jarnohermans94@gmail.com>
2016-04-24 23:02:23 +0200

    Vragen uit API halen en swipen af

--- App/projectwerking/Assets/Scripts/PaperController.cs
+++ App/projectwerking/Assets/Scripts/PaperController.cs
@@ -7,31 +7,37 @@ using UnityEngine.UI;
 
 public class PaperController : MonoBehaviour
 {
-
-  public RectTransform paperPrefab;
-  public Text paperTextbox;
-  public Text titleTextbox;
+  public GameObject tMeshPrefab, tMeshTitlePrefab;
   public Transform startposition, focusposition, endposition;
-  public float speed = 0.5f;
+  public bool ListIsReady = false;
 
-  RectTransform currentPaper;
+  TextMesh tMeshText, tMeshTitle;
+  GameObject currentPaper, newPaper;
   int currentQuestionNr = 0;
   //string url = "http://semicolon.multimediatechnology.be/projecten";
   string testurl = "http://jsonplaceholder.typicode.com/posts";
 
   string titleText = "Vraag: ";
   string questionValue = "body";
-  string currentQuestionString;
   List<string> QuestionList = new List<string>();
 
   int numberOfQuestions = 0;
   static WWW www;
   JsonData textData;
 
+  void Awake() {
+    tMeshText = tMeshPrefab.GetComponent<TextMesh>();
+    tMeshTitle = tMeshTitlePrefab.GetComponent<TextMesh>();
+  }
+
+  public void SetBeginValues() {
+    //currentQuestionNr = 0;
+    tMeshText.text = ResolveTextSize(QuestionList[currentQuestionNr], 24);
+    tMeshTitle.text = titleText + (currentQuestionNr + 1).ToString();
+  }
 
   IEnumerator Start()
   {
-    currentQuestionNr++;
     www = new WWW(testurl);
     yield return www;
     if (www.error == null)
@@ -44,53 +50,85 @@ public class PaperController : MonoBehaviour
         QuestionList.Add(textData[i][questionValue].ToString());
       }
       Debug.Log(QuestionList.Count);
+      ListIsReady = true;
     }
     else
     {
       Debug.Log("ERROR: " + www.error);
     }
-
   }
 
+ private string ResolveTextSize(string input, int lineLength)
+  {
+
+    // Split string by char " "         
+    string[] words = input.Split(" "[0]);
+
+    // Prepare result
+    string result = "";
+
+    // Temp line string
+    string line = "";
+
+    // for each all words        
+    foreach (string s in words)
+    {
+      // Append current word into line
+      string temp = line + " " + s;
+
+      // If line length is bigger than lineLength
+      if (temp.Length > lineLength)
+      {
+
+        // Append current line into result
+        result += line + "\n";
+        // Remain word append into new line
+        line = s;
+      }
+      // Append current word into current line
+      else {
+        line = temp;
+      }
+    }
+
+    // Append last line into result        
+    result += line;
+
+    // Remove first " " char
+    return result.Substring(1, result.Length - 1);
+  }
+
+
   public void setText() {
-    paperTextbox.text = QuestionList[currentQuestionNr];
-    titleTextbox.text = titleText + currentQuestionNr.ToString();
     currentQuestionNr++;
+    string temp = ResolveTextSize(QuestionList[currentQuestionNr],24);
+    tMeshText.text = temp;
+    tMeshTitle.text = titleText + (currentQuestionNr + 1).ToString();
     Debug.Log(currentQuestionNr);
   }
 
-  public void setFirstPaper() {
-    RectTransform newPaper;
-    if (currentPaper == null)
-    {
-      setText();
-      newPaper = (RectTransform)Instantiate(paperPrefab, focusposition.position, transform.rotation);
-
-      currentPaper = newPaper;
-
-    }
+  public void DestroyCurrentPaper() {
+    Destroy(currentPaper);
   }
 
-  public void SwipeNext()
+  public void setCurrentPaper() {
+    currentPaper = newPaper;
+  }
+
+  public void createNewPaper()
   {
-    RectTransform newPaper;
-      newPaper = (RectTransform)Instantiate(paperPrefab, startposition.position, transform.rotation);
-      newPaper.transform.localPosition = Vector3.Lerp(focusposition.position, endposition.position, speed);
-
-      setText();
-      currentPaper.transform.localPosition = Vector3.Lerp(startposition.position, focusposition.position, speed);
-      Destroy(currentPaper);
-      currentPaper = newPaper;
-      
-    }
-
-
+    newPaper = (GameObject)Instantiate(tMeshPrefab, startposition.position, transform.rotation);
+    setText();
   }
 
-  /*
-  public void SwipePrevious() {
-    currentQuestionNr--;
-    currentPaper.transform.position = Vector3.Lerp(focusposition.position, endposition.position, speed);
-  }*/
+  public void moveNewPaper(float step) {
+    newPaper.transform.localPosition = Vector3.Lerp(startposition.position, focusposition.position, step);
+  }
+
+  public void moveFocusPaper(float step)
+  {  
+    currentPaper.transform.localPosition = Vector3.Lerp(focusposition.position, endposition.position, step);
+  }
+}
 
 
