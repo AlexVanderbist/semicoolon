ee1cb87f2814d787c1f1c0afd039fe59850acc88
AlexVanderbist <AlexVanderbist@gmail.com>
2016-05-11 19:00:56 +0200

    added form

--- web/resources/views/backend/projects/proposals/index.blade.php
+++ web/resources/views/backend/projects/proposals/index.blade.php
@@ -3,30 +3,34 @@
 @section('title', 'Stellingen voor ' . $project->name)
 
 @section('content')
-    <a href="{{ route('backend.projects.index') }}" class="btn btn-default">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Overzicht projecten
-    </a>
-    <a href="{{ route('backend.projects.index') }}" class="btn btn-default">
-        <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Project aanpassen
-    </a>
-
-    <div class="panel panel-default">
-        <!-- List group -->
-        <ul class="list-group">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
-
-        <div class="panel-footer">
-            <p>...</p>
-        </div>
+    <div class="btn-group" role="group" style="margin-bottom:15px">
+        <a href="{{ route('backend.projects.index') }}" class="btn btn-default">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> Overzicht projecten
+        </a>
+        <a href="{{ route('backend.projects.edit', $project->id) }}" class="btn btn-default">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Project aanpassen
+        </a>
     </div>
 
-    @foreach($proposals as $proposal)
-        <div></div>
-    @endforeach
+    <div class="panel panel-default">
+
+        <div class="panel-body">
+            <p>Nieuwe stelling toevoegen</p>
+        </div>
+
+        <table class="table table-hover">
+            <tbody>
+                @foreach($proposals as $proposal)
+                    <tr>
+                        <td>{{$proposal->description}}</td>
+                        <td>{{$proposal->type}}</td>
+                        <td>
+                            ja: 5 | nee: 8
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
 @endsection
