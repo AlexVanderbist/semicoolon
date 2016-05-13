5432f2e792af2f5900a23dfaff6d5d88f0e4b164
AlexVanderbist <AlexVanderbist@gmail.com>
2016-05-13 09:29:29 +0200

    new youtube getter/setter

--- web/app/Project.php
+++ web/app/Project.php
@@ -43,6 +43,15 @@ class Project extends Model
         return $this->hasMany(Proposal::class);
     }
 
+    public function setYoutubeUrlAttribute($value) {
+        parse_str( parse_url( $value, PHP_URL_QUERY ), $id );
+        $this->attributes['youtube_url'] = $id['v'];
+    }
+
+    public function getYoutubeUrlAttribute () {
+        return 'https://www.youtube.com/watch?v=' . $this->youtube_url;
+    }
+
     public function youtubeID($url)
     {
         parse_str( parse_url( $url, PHP_URL_QUERY ), $id );
