4d3511dc26c338d605df89ef6c4b811226c2625b
AlexVanderbist <alex.vanderbist@gmail.com>
2016-05-18 19:44:59 +0200

    finally fixed user api call

--- web/app/Http/Controllers/API/ProjectsController.php
+++ web/app/Http/Controllers/API/ProjectsController.php
@@ -8,34 +8,48 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use App\Project;
+use App\Proposal;
 
 class ProjectsController extends Controller
 {
 	protected $projects;
+	protected $proposals;
 
-	public function __construct(Project $projects) {
+	public function __construct(Project $projects, Proposal $proposals) {
 
 		$this->projects = $projects;
-
+		$this->proposals = $proposals;
 	}
 
-    public function index() {
+    public function index() 
+    {
 
-    	$projects = $this->projects->with('theme', 'creator')->get();
+		try {
+    		$projects = $this->projects->with('theme', 'creator')->get();
+		}
+		catch (NotFoundHttpException $e)
+		{
+		    $projects = [];
+		}
+
 
     	return response()->json(compact('projects'));
     }
 
-    public function getProposals(Project $project) {
-        $proposals = $project->proposals;
+    public function getProposals($id) {
+		$project = $this->projects->find($id);
+		if($project) {
+   			$proposals = $project->proposals;
+		} else $proposals = [];
+
         return response()->json(compact('proposals'));
     }
 
-    public function getProposalsForUser(Project $project) {
-        $proposals = $project->with('proposals')->whereHas('proposals.opinions', function($q)
-        {
-            $q->where('user_id', '!=', \Auth::user()->id);
-        });
+    public function getProposalsForUser($projectId) {
+    	$proposals = $this->proposals->where('project_id', $projectId)->has('opinions', '<', 1)->orWhereHas('opinions', function($q)
+	        {
+	            $q->where('user_id', '!=', \Auth::user()->id);
+	        })->get();
 
         return response()->json(compact('proposals'));
     }
