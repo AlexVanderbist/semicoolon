a3c7a9b7a93dfe351434524c8d8382df8fcaf14c
AlexVanderbist <AlexVanderbist@gmail.com>
2016-05-20 00:50:47 +0200

    added header image in appends on model

--- web/app/Project.php
+++ web/app/Project.php
@@ -23,6 +23,8 @@ class Project extends Model
         'youtube_url'
     ];
 
+    protected $appends = ['header_image', 'youtube_id'];
+
     public function creator()
     {
         return $this->belongsTo(User::class, 'project_creator');
@@ -53,12 +55,6 @@ class Project extends Model
         return $this->hasMany(Opinion::class);
     }
 
-    public function getHeaderImageAttribute() {
-        $image = $this->images()->where('is_header', 1)->first();
-        if(!$image) $image = $this->images()->first();
-        return $image;
-    }
-
     public function setYoutubeUrlAttribute($value) {
         parse_str( parse_url( $value, PHP_URL_QUERY ), $id );
         $this->attributes['youtube_url'] = $id['v'];
@@ -70,5 +66,11 @@ class Project extends Model
 
     public function getYoutubeIdAttribute () {
         return $this->attributes['youtube_url'];
+    }
+
+    public function getHeaderImageAttribute() {
+        $image = $this->images()->where('is_header', 1)->first();
+        if(!$image) $image = $this->images()->first();
+        return $image;
     }
 }
