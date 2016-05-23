20a528180883308f8bb943c5c7f2825779410d84
brentbelmans <brentbelmans@hotmail.com>
2016-05-23 19:58:12 +0200

    random stempel afdrukken

---
+++ App/projectwerking/Assets/Scripts/AndroidBackButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class AndroidBackButton : MonoBehaviour {
+
+    private bool isPause = false;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if( Input.GetKeyUp(KeyCode.Escape))
+        {
+            Debug.Log("BackButton clicked");
+            isPause = !isPause;
+            if (isPause)
+            {
+                Time.timeScale = 0;
+                Debug.Log("looking for pause panel");
+                GameObject.Find("PausePanel").SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+                
+        }
+	
+	}
+}
