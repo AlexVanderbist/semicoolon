b28db9556ecebacf9a68d27cfed49636e3c6eb32
AlexVanderbist <AlexVanderbist@gmail.com>
2016-05-30 03:11:31 +0200

    added windows to map and custom icons

--- web/public/angular/scripts/controllers/projectController.js
+++ web/public/angular/scripts/controllers/projectController.js
@@ -14,12 +14,25 @@
         $scope.projectsFiltered = [];
         $scope.filter = [];
 
+        // returns google maps icon object with symbol in given color
+        var iconSymbol = function (color) {
+            return {
+                path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+                fillColor: color,
+                fillOpacity: 1,
+                strokeColor: '#000',
+                strokeWeight: 2,
+                scale: 1,
+           };
+        };
+
         // Add lat en lng in full
         angular.forEach($scope.projects, function(value, key) {
             value.latitude = value.lat;
             value.longitude = value.lng;
+            value.icon = iconSymbol(value.theme.hex_color);
+            console.log(value);
         });
-
 
         $scope.$watchCollection('filter', function(newValue) {
             console.log('dddd');
--- web/public/angular/scripts/controllers/projectMapController.js
+++ web/public/angular/scripts/controllers/projectMapController.js
@@ -4,14 +4,41 @@
 
     angular
         .module('antwerpApp')
-        .controller('projectMapController', function ($scope) {
+        .controller('projectMapController', function ($scope, uiGmapGoogleMapApi) {
         	
         	$scope.projectsFiltered = $scope.$parent.projectsFiltered;
-            $scope.map = { center: { latitude: 51.218686, longitude: 4.417458 }, zoom: 14 };
+            $scope.map = { 
+                center: { 
+                    latitude: 51.218686, 
+                    longitude: 4.417458 
+                }, 
+                zoom: 14,
+                markersEvents: {
+                    click: function(marker, eventName, model) {
+                        console.log('Click marker');
+                        $scope.map.window.model = model;
+                        $scope.map.window.show = true;
+                    }
+                },
+                window: {
+                    marker: {},
+                    show: false,
+                    closeClick: function() {
+                        this.show = false;
+                    },
+                    options: {} // defined when map is loaded
+                }
+            };
+
+            uiGmapGoogleMapApi.then(function(maps) {
+                // offset to fit the custom icon
+                $scope.map.window.options.pixelOffset = new google.maps.Size(0, -35, 'px', 'px');
+            });
 
 			$scope.$parent.$watchCollection('projectsFiltered', function(newValue) {
 	            $scope.projectsFiltered = newValue;
 	        });
+
         });
     
 })();
