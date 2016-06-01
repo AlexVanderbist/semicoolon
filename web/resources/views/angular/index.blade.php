d085a4e0a66b26c1a5e977b3b2acf15e8e55c7d3
AlexVanderbist <AlexVanderbist@gmail.com>
2016-06-01 13:35:13 +0200

    fixed checkboxes checked by default

--- web/resources/views/angular/index.blade.php
+++ web/resources/views/angular/index.blade.php
@@ -61,14 +61,16 @@
     </body>
 
     <!-- Application Dependencies - Dont forget to add .min in production build -->
-    <script src="{!! asset('node_modules/lodash/lodash.js') !!}"></script>
+    <script src="{!! asset('node_modules/lodash/lodash.min.js') !!}"></script>
     <script src="{!! asset('node_modules/angular/angular.js') !!}"></script>
     <script src="{!! asset('node_modules/angular-ui-router/release/angular-ui-router.js') !!}"></script>
-    <script src="{!! asset('node_modules/satellizer/satellizer.js') !!}"></script>
+    <script src="{!! asset('node_modules/satellizer/satellizer.min.js') !!}"></script>
     <script src="{!! asset('node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js') !!}"></script>
-    <script src="{!! asset('node_modules/angular-sanitize/angular-sanitize.js') !!}"></script>
-    <script src="{!! asset('node_modules/angular-simple-logger/dist/angular-simple-logger.js') !!}"></script>
-    <script src="{!! asset('node_modules/angular-google-maps/dist/angular-google-maps.js') !!}"></script>
+    <script src="{!! asset('node_modules/angular-sanitize/angular-sanitize.min.js') !!}"></script>
+    <script src="{!! asset('node_modules/angular-simple-logger/dist/angular-simple-logger.min.js') !!}"></script>
+    <script src="{!! asset('node_modules/angular-google-maps/dist/angular-google-maps.min.js') !!}"></script>
+    <script src="https://www.youtube.com/iframe_api"></script>
+    <script src="{!! asset('node_modules/angular-youtube-embed/dist/angular-youtube-embed.min.js') !!}"></script>
 
     <!-- Application Scripts -->
     <script src="{!! asset('angular/scripts/app.js') !!}"></script>
