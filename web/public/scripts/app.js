835da9438a70f3927a87a4a29e27ef36817f0298
AlexVanderbist <AlexVanderbist@gmail.com>
2016-06-01 16:45:07 +0200

    added more filters

--- web/public/scripts/app.js
+++ web/public/scripts/app.js
@@ -161,10 +161,35 @@
                 $state.go('projects');
             });
         })
+        
         .filter('dateToISO', function() {
             return function(input) {
                 input = new Date(input).toISOString();
                 return input;
+            };
+        })
+
+        .filter('htmlToPlaintext', function() {
+            return function(text) {
+                return  text ? String(text).replace(/<[^>]+>/gm, '') : '';
+            };
+        })
+
+        .filter('truncate', function () {
+            return function (text, length, end) {
+                if (isNaN(length))
+                    length = 10;
+
+                if (end === undefined)
+                    end = "...";
+
+                if (text.length <= length || text.length - end.length <= length) {
+                    return text;
+                }
+                else {
+                    return String(text).substring(0, length-end.length) + end;
+                }
+
             };
         });
 })();
