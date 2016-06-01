8f22e9c3a2f4831ff821544642958dec96da6c14
rubends <rubendeswaef@gmail.com>
2016-06-01 13:18:33 +0100

    added favicons

--- web/resources/views/layouts/backend.blade.php
+++ web/resources/views/layouts/backend.blade.php
@@ -26,6 +26,21 @@
     <link href="{!! asset('trumbowyg/ui/trumbowyg.min.css') !!}" rel="stylesheet">
     <link href="{!! asset('/css/style.css') !!}" rel="stylesheet">
 
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#CF0039">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#cf0039">
+
 </head>
 <body id="app-layout">
     <nav class="navbar navbar-default">
