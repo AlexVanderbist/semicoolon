bd9e7e7d3792a9b4c49bc1bb8e01756a58dd4e34
AlexVanderbist <AlexVanderbist@gmail.com>
2016-06-02 07:32:04 +0200

    added comments

--- web/public/scripts/controllers/projectController.js
+++ web/public/scripts/controllers/projectController.js
@@ -19,13 +19,6 @@
             };
             
             // bind some scope stuff
-            $scope.map = { 
-                center: { 
-                    latitude: $scope.project.lat, 
-                    longitude: $scope.project.lng 
-                }, 
-                zoom: 15
-            };
 
             $scope.project = project.data.project;
             $scope.project.latitude = $scope.project.lat;
@@ -33,6 +26,13 @@
             $scope.project.icon = iconSymbol($scope.project.theme.hex_color);
             $scope.project.opinions = [];
             $scope.opinionsInitLoading = true;
+            $scope.map = { 
+                center: { 
+                    latitude: $scope.project.lat, 
+                    longitude: $scope.project.lng 
+                }, 
+                zoom: 15
+            };
 
             console.log($scope.project);
 
