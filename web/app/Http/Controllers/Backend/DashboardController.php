c2fd37c23d3cdd1ade2b3da2fa9c74894b6fa780
rubends <rubendeswaef@gmail.com>
2016-06-04 17:18:58 +0200

    dashboard

--- web/app/Http/Controllers/Backend/DashboardController.php
+++ web/app/Http/Controllers/Backend/DashboardController.php
@@ -4,12 +4,35 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\User;
+use App\Project;
+use App\ProposalOpinion;
+use App\Opinion;
+use App\Proposal;
 
 class DashboardController extends Controller
 {
-    public function index() {
+	protected $users;
+	protected $projects;
+	protected $proposalopinions;
+	protected $opinions;
+	protected $proposals;
+	public function __construct(Proposal $proposals, User $users, Project $projects, ProposalOpinion $proposalopinions, Opinion $opinions) {
+		$this->users = $users;
+		$this->projects = $projects;
+		$this->proposalopinions = $proposalopinions;
+		$this->opinions = $opinions;
+		$this->proposals = $proposals;
+		parent::__construct();
+	}
 
-        return view('backend.dashboard');
+    public function index() {
+    	$users = $this->users->get();
+    	$projects = $this->projects->count();
+    	$proposalopinions = $this->proposalopinions->count();
+    	$opinions = $this->opinions->count();
+    	$proposals = $this->proposals->count();
+        return view('backend.dashboard', compact('users', 'user', 'projects', 'proposalopinions', 'opinions', 'proposals'));
     }
 
 }
